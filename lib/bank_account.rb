13af692e4a95b3d5b99665865c4a43f04dda7d59
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-02 20:00:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,18 @@
 class BankAccount
+attr_reader :name, :balance, :status
 
+def initialize(name, balance =1000, status ='open')
+ @name = name  
+ @balance = balance
+ @status = status
+ end
+ 
+  def deposit(money)
+    @balance << money
+  end 
+  
+  def display_balance
+    
+  end 
+ 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
   # your code here
+  
+  attr_reader :status
+  attr_accessor :amount, :receiver, :sender 
+  
+  def initialize(sender,receiver,amount, status = 'pending')
+    @sender = sender
+    @status = status
+    @amount = amount
+    @receiver = receiver
+    @sender = sender   
+  end 
+  
+  
 end
